e70b4dc01bb2480d7cb3c50ef5d0a9fc6495d1ff
Ubuntu <ubuntu@ip-172-31-22-33.eu-north-1.compute.internal>
2024-05-29 09:07:56 +0000

    this is the change in script file

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Define Docker Hub credentials
+DOCKER_USERNAME="nafilatest"
+DOCKER_PASSWORD="Nafitest-user@2"
+DOCKER_REPOSITORY="dev"
+DOCKER_TAG="latest"
+
+# Log in to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
 # Build Docker image
-docker build -t  nafi_test_1 .
+docker build -t "${DOCKER_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}" .
+
+# Push Docker image to Docker Hub
+docker push "${DOCKER_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# Pull the Docker image
-docker pull nafi_test_1
+# Define Docker Hub credentials and image details
+DOCKER_USERNAME="nafilatest"
+DOCKER_PASSWORD="Nafitest-user@2"
+DOCKER_REPOSITORY="dev"
+DOCKER_IMAGE="nafi_test_1"
+DOCKER_TAG="latest"  # Update this if using a different tag
 
-# Run the Docker container
-docker run -d -p 8082:8082 nafi_test_1
+# Log in to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+# Pull Docker image from Docker Hub
+docker pull "${DOCKER_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+
+
+# Run new container, mapping port 8082 to the container's port
+docker run -d --name nafi_container -p 8082:8082 "${DOCKER_USERNAME}/${DOCKER_REPOSITORY}:${DOCKER_TAG}"
+
 
